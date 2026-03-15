574d53c31fd099fb4454359b73bf9b115593030d
Егор <egorhacker05@gmail.com>
2026-03-15 18:08:00 +1000

    feature: Added markdown text support

--- server/recruiter-bot/source/main.py
+++ server/recruiter-bot/source/main.py
@@ -9,6 +9,6 @@ context: TeleBot = TeleBot("8738837682:AAHRka74Olp0_dCqG9btSyktThkn79yjr8Y")
 	commands = ['start', 'help']
 )
 def handle_start_message(message : Message) -> None:
-	context.send_message(chat_id = message.chat.id, text = WELCOME_TEXT, reply_markup = WELCOME_TEXT_MARKUP)
+	context.send_message(message.chat.id, WELCOME_TEXT, "MARKDOWN", reply_markup = WELCOME_TEXT_MARKUP)
 
 if __name__ == "__main__": context.infinity_polling()
