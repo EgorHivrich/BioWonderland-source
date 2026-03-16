f1f14588c9a37e34556485bbe83956d688558fc5
Егор <egorhacker05@gmail.com>
2026-03-16 22:40:52 +1000

    chore: Adding service script moving hook on startup

--- server/scripts/startup.sh
+++ server/scripts/startup.sh
@@ -1,5 +1,11 @@
 #!bin/bash
 
+SERVICE_SCRIPT_PATH="/etc/systemd/system"
+
+if [ -f "$SERVICE_SCRIPT_PATH/startup.service" ]; then
+    cp recruiter-bot/startup.service $(SERVICE_SCRIPT_PATH)
+fi
+
 systemctl daemon-reload
 systemctl enable startup.service
 
