67a4a0f1df0617386b731aceae0b13f1c90f6ebd
Егор <egorhacker05@gmail.com>
2026-03-17 01:58:29 +1000

    fix: Fixed constant moving of service file

--- server/scripts/startup.sh
+++ server/scripts/startup.sh
@@ -2,7 +2,7 @@
 
 SERVICE_SCRIPT_PATH="/etc/systemd/system"
 
-if [ -f "$SERVICE_SCRIPT_PATH/startup.service" ]; then
+if [ ! -f "$SERVICE_SCRIPT_PATH/startup.service" ]; then
     cp recruiter-bot/startup.service $SERVICE_SCRIPT_PATH
 fi
 
