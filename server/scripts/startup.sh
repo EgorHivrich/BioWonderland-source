cf5a0fac03b97d7aaa93f4f3d868bb47575ec550
Егор <egorhacker05@gmail.com>
2026-03-17 01:22:53 +1000

    fix: Fixed bad bash issue

--- server/scripts/startup.sh
+++ server/scripts/startup.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/sh
 
 SERVICE_SCRIPT_PATH="/etc/systemd/system"
 
 if [ -f "$SERVICE_SCRIPT_PATH/startup.service" ]; then
-    cp recruiter-bot/startup.service $(SERVICE_SCRIPT_PATH)
+    cp recruiter-bot/startup.service $SERVICE_SCRIPT_PATH
 fi
 
 systemctl daemon-reload
